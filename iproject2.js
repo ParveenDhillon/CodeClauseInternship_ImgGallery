5952464a6555acd8e1230f93448f3c2b97687eb0
Parveen Dhillon <147579630+ParveenDhillon@users.noreply.github.com>
2024-03-07 21:39:16 +0530

    Add files via upload

---
+++ iproject2.js
@@ -0,0 +1,50 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Weapon",
+      artist: "Komal chaudhary",
+      img: "Weapon.jpg",
+      src: "Weapon"
+    },
+    {
+      name: "Dekha ek Khawab",
+      artist: "Kishore Kumar",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Time Table",
+      artist: "Kulwinder Billa",
+      img: "Time-table",
+      src: "Time-table"
+    },
+    {
+      name: "Bapu Zimidar",
+      artist: "Jassie Gill",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Jinna Jinna",
+      artist: "Gurnam Bhullar",
+      img: "jinna-jinna",
+      src: "music-1"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
